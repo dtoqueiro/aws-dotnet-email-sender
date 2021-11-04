aeb0ab4bed11f45bfa1cac43444b763724815baf
Daniel Lins Toqueiro <dtoqueiro@gmail.com>
2021-11-04 13:57:25 -0300

    Project start
    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aws_dotnet_email_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
